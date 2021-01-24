717358d37b0f5d6589c8c7a3620bd854de0098f5
tibraj <tibraj94@gmail.com>
2021-01-24 16:06:23 -0500

    group class

--- app.js
+++ app.js
@@ -18,3 +18,35 @@ class Vector {
 console.log(new Vector(1,2).plus(new Vector(2,3)));
 console.log(new Vector(1,2).minus(new Vector(2,3)));
 console.log(new Vector(3,4).length);
+
+//Group Type
+class Group {
+    constructor() {
+        this.members = [];
+    }
+    add(value) {
+        if(!this.has(value)) {
+            this.members.push(value);
+        }
+    }
+    delete(value) {
+        this.members = this.members.filter(v => v!== value);
+    }
+    has(value) {
+        return this.members.includes(value);
+    }
+    static from(collection) {
+        let group = new Group;
+        for(let value of collection) {
+            group.add(value);
+        }
+        return group;
+    }
+}
+
+let group = Group.from([10,20]);
+console.log(group,has(10));
+console.log(group.has(30));
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
