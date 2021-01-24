a5db091d7dd88f9da3cedbd00e95c708fc49ff61
tibraj <tibraj94@gmail.com>
2021-01-24 16:09:36 -0500

    group iterator

--- app.js
+++ app.js
@@ -42,6 +42,25 @@ class Group {
         }
         return group;
     }
+    [Symbol.iterator]() {
+        return new GroupIterator(this);
+    }
+}
+
+class GroupIterator {
+    constructor(group) {
+        this.group = group;
+        this.position = 0;
+    }
+    next() {
+        if(this.position >= this.group.members.length) {
+            return {done: true};
+        } else {
+            let result = {value: this.group.members[this.positio], done: false};
+            this.position++;
+            return result;
+        }
+    }
 }
 
 let group = Group.from([10,20]);
@@ -50,3 +69,6 @@ console.log(group.has(30));
 group.add(10);
 group.delete(10);
 console.log(group.has(10));
+for(let value of Group.from(["a", "b", "c"])) {
+    console.log(value);
+}
