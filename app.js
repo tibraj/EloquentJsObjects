6254b779ad4cd7886db77c8a92908592c7d5a0fa
tibraj <tibraj94@gmail.com>
2021-01-24 15:29:54 -0500

    vector class

---
+++ app.js
@@ -0,0 +1,20 @@
+//Vector Type
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(other) {
+        return new Vector(this.x + other.x, this.y + other.y);
+    }
+    minus(other) {
+        return new Vector(this.x - other.x, this.y - other.y);
+    }
+    get length() {
+        return Math.sqrt((this.x * this.x ) + (this.y * this.y));
+    }
+}
+
+console.log(new Vector(1,2).plus(new Vector(2,3)));
+console.log(new Vector(1,2).minus(new Vector(2,3)));
+console.log(new Vector(3,4).length);
